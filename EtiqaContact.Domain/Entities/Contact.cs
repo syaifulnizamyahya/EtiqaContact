09c7d172f0b8c47bc3cfd557ce4acfabcf018493
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2024-09-01 13:43:18 +0800

    Contact model. Remove restriction. Mail is now Email.

---
+++ EtiqaContact.Domain/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtiqaContact.Domain.Entities;
+
+public class Contact
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<string> Skillsets { get; set; }
+    public List<string> Hobbies { get; set; }
+}
